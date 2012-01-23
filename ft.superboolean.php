a84c9cff855b9ff46b239419b37133fe24ed0503
Phil Pelanne <phil@insidenewcity.com>
2012-01-23 14:41:29 -0500

    added ability to save custom true/false labels

--- ft.superboolean.php
+++ ft.superboolean.php
@@ -15,14 +15,15 @@ class Superboolean_ft extends EE_Fieldtype {
 	/**
 	 * Display Field in Control Panel
 	 *
+	 * @access	public
 	 * @param string $data 
-	 * @return void
+	 * @return string $output
 	 */
     function display_field($data)
     {
 		$options = array(
-		                  '0|'.$this->EE->input->get('channel_id')  => 'No',
-		                  '1|'.$this->EE->input->get('channel_id')    => 'Yes',
+		                  '0|'.$this->EE->input->get('channel_id')  => $this->settings['false_label'],
+		                  '1|'.$this->EE->input->get('channel_id')    => $this->settings['true_label'],
 		);
 		$output = form_dropdown($this->field_name, $options, $data);
 		
@@ -33,6 +34,7 @@ class Superboolean_ft extends EE_Fieldtype {
 	/**
 	 * Unset all the other entries in the channel after save
 	 *
+	 * @access	public
 	 * @param string $data 
 	 * @return void
 	 */
@@ -45,7 +47,48 @@ class Superboolean_ft extends EE_Fieldtype {
 				$this->EE->db->update('exp_channel_data', array($this->field_name => ''));
 			}
 		}
+	}
+	
+	
+	/**
+	 * Display Settings Screen - allow admin to set meaningful lables for boolean
+	 *
+	 * @access	public
+	 * @return	default global settings
+	 *
+	 */
+	function display_settings($data)
+	{
+		$true_label	= isset($data['true_label']) ? $data['true_label'] : 'Yes';
+		$false_label	= isset($data['false_label']) ? $data['false_label'] : 'No';
+
+		$this->EE->table->add_row(
+			lang('True Label','true_label'),
+			form_input('true_label', $true_label)
+		);
 		
+		$this->EE->table->add_row(
+			lang('False Label','false_label'),
+			form_input('false_label', $false_label)
+		);
 		
 	}
+	
+	// --------------------------------------------------------------------
+
+	/**
+	 * Save Settings - saves individual settings to database
+	 *
+	 * @access	public
+	 * @return	field settings
+	 *
+	 */
+	function save_settings($data)
+	{
+		return array(
+			'true_label'	=> $this->EE->input->post('true_label'),
+			'false_label'	=> $this->EE->input->post('false_label'),
+		);
+	}
+	
 }
